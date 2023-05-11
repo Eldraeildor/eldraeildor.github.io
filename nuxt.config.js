f01a8830666081a3df8674d380184f9539375dc0
Ellena Fernandez <Eldraeildor@users.noreply.github.com>
2023-05-11 11:59:15 +0200

    I should now be able to start coding this site

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - eldraeildor.github.io',
-    title: 'eldraeildor.github.io',
+    titleTemplate: '%s',
+    title: 'Ellena\'s Zone (WIP)',
     htmlAttrs: {
       lang: 'en'
     },
--- pages/inspire.vue
+++ pages/inspire.vue
@@ -7,10 +7,10 @@
         class="mb-5"
       >
       <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
+        &#8220;First, code problems. Then, solve the issues.&#8221;
         <footer>
           <small>
-            <em>&mdash;John Johnson</em>
+            <em>&mdash;Me</em>
           </small>
         </footer>
       </blockquote>
